735504c23e3b59a53332f660775ab85bd035b381
yyj <2695450496@qq.com>
2022-10-25 22:34:33 +0800

    Site updated: 2022-10-25 22:34:33

---
+++ js/Lete.js
@@ -0,0 +1,8 @@
+$(document).ready(function(e){
+    $('.copyright').html('©2022 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 生日快乐，闫怡静');
+})
+var app = document.getElementById('footer');
+app.removeAttribute('id')
+
+// 判断移动端设备
+ 
---
+++ js/crash_cheat.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/joke.ico");
+         document.title = 'w(°A°)w不要走！再看看嘛！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = ' ( ๑•̀ㅂ•́) ✧不要再离开我啊～';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
